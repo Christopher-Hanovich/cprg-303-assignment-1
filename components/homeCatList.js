dbde37737f91749235ad21bccfab8d89a90dcc6f
Chris Hanlon <christopher.hanlon1@gmail.com>
2025-10-27 12:28:08 -0600

    Changes
    changes to make it better

--- components/homeCatList.js
+++ components/homeCatList.js
@@ -1,12 +1,21 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import categories from "./categories.json";
 import HomeCategory from "./homeCategory";
 export default function homeCatList() {
   return (
-    <View>
+    <View Style={Styles.container}>
       {categories.map((category, index) => (
         <HomeCategory key={index} category={category} />
       ))}
     </View>
   );
 }
+const Styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    background: "red",
+    color: "blue",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+});
